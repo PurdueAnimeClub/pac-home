2c977e8c961ad11f97a5f1f6437426d37b7ab935
Sargun Vohra <sargunster@gmail.com>
2014-07-17 00:10:55 -0400

    Organization, commenting, and formatting.

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 
   <body>
 
-	<!-- Fixed navbar -->
+	<!-- Navbar -->
 	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
 	  <div class="container">
 		<div class="navbar-header">
@@ -39,7 +39,6 @@
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 		  </button>
-		  <!--<a class="navbar-brand" href="#">Purdue Anime Club</a>-->
 		</div>
 		<div class="navbar-collapse collapse">
 		  <ul class="nav navbar-nav">
@@ -50,167 +49,185 @@
 			<li><a href="#cosplay">Cosplay</a></li>
 			<li><a href="#forum">Forum</a></li>
 			<li><a href="#contact">Contact</a></li>
-			<li class="dropdown">
-			  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Theme<span class="caret"></span></a>
-			  <ul class="dropdown-menu" role="menu">
-				<li><a href="#">Light</a></li>
-				<li><a href="#">Dark</a></li>
-			  </ul>
-			</li>
+			<li><a href="#">Switch Theme</a></li>
 		  </ul>
-		</div><!--/.nav-collapse -->
+		</div>
 	  </div>
 	</div>
 
-<div class="container">
-	<div class="row clearfix">
-		<div class="col-md-8 column">
-			<div class="well">
-				<h2>
-					Announcements
-				</h2>
-				<p><small>July 2, 9:48PM</small></p>
-				<p>
-					Tomorrow's meeting of Summer PAC will be starting at 7pm, but we will be in a different locale this week. Chuck has graciously volunteered to hold the meeting at his place so that we can experience the concert sequence in White Album S2 in full surround sound on an excellent sound system. Feel free to show up at 6pm as he is making teriyaki burgers. If you need a ride there, be sure to let me know. I'll be picking up people out in front of the Memorial Union at 6pm. If you're driving yourself, I'll be including information in the email for how to get to his place.
-Hope to see you there!
-				</p>
-				<a href="#news"><small>Previous announcements</small></a>
-			</div>
-			<div class="well">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="thumbnail">
-							<img alt="600x200" src="assets/img/pac-thumbnail.jpg" />
-							<div class="caption">
-								<h3>
-									PAC
-								</h3>
-								<p>
-									Purdue Anime Club (PAC) is Purdue University's premier anime viewing club.  PAC meets once a week on Thursdays from 7 p.m. to around 11:30 p.m. and is typically held in Stewart Center.
-								</p>
-								<p>
-									<a class="btn btn-primary" href="#pac">Details</a>
-								</p>
+	<div class="container">
+		<div class="row clearfix">
+			
+			<!-- Main Content -->
+			<div class="col-md-8 column">
+				<!-- PAC Announcements -->
+				<div class="well">
+					<h2>
+						Announcements
+					</h2>
+					<p><small>July 2, 9:48PM</small></p>
+					<p>
+						Tomorrow's meeting of Summer PAC will be starting at 7pm, but we will be in a different locale
+						this week. Chuck has graciously volunteered to hold the meeting at his place so that we can
+						experience the concert sequence in White Album S2 in full surround sound on an excellent sound
+						system. Feel free to show up at 6pm as he is making teriyaki burgers. If you need a ride there,
+						be sure to let me know. I'll be picking up people out in front of the Memorial Union at 6pm. If
+						you're driving yourself, I'll be including information in the email for how to get to his place.
+					</p>
+					<p>
+						Hope to see you there!
+					</p>
+					<a href="#news"><small>Previous announcements</small></a>
+				</div>
+				
+				<!-- PAC Theater -->
+				<div class="well">
+					<h2>
+						PAC Theater
+					</h2>
+					<div class="responsive-video">
+						<iframe src="http://www.youtube.com/embed/kUDs5pOyWkk" frameborder="0" allowfullscreen=""></iframe>
+					</div>
+				</div>
+				
+				<!-- PAC Sub groups -->
+				<div class="well">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="600x200" src="assets/img/pac-thumbnail.jpg" />
+								<div class="caption">
+									<h3>
+										PAC
+									</h3>
+									<p>
+										Purdue Anime Club (PAC) is Purdue University's premier anime viewing club.  PAC
+										meets once a week on Thursdays from 7 p.m. to around 11:30 p.m. and is typically
+										held in Stewart Center.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#pac">Details</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="600x200" src="assets/img/classic-thumbnail.jpg" />
+								<div class="caption">
+									<h3>
+										PAC Classic
+									</h3>
+									<p>
+										PAC Classic is our sub-group dedicated to showing those classic anime series,
+										ovas, and movies. Whether you missed them the first time around or if you enjoy
+										rewatching your favorite old shows, this is the place to be.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#classic">Details</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="600x200" src="assets/img/27-thumbnail.jpg" />
+								<div class="caption">
+									<h3>
+										PAC 27+
+									</h3>
+									<p>
+										PAC 27+ is our sub-group that shows anime that otherwise would not fit in the
+										regular lineup due to the length of the programs. It usually features shows
+										which are greater than 27 episodes in length.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#27">Details</a>
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-4">
-						<div class="thumbnail">
-							<img alt="600x200" src="assets/img/classic-thumbnail.jpg" />
-							<div class="caption">
-								<h3>
-									PAC Classic
-								</h3>
-								<p>
-									PAC Classic is our sub-group dedicated to showing those classic anime series, ovas, and movies. Whether you missed them the first time around or if you enjoy rewatching your favorite old shows, this is the place to be.
-								</p>
-								<p>
-									<a class="btn btn-primary" href="#classic">Details</a>
-								</p>
-							</div>
-						</div>
+				</div>
+			</div>
+			
+			<!-- Sidebar -->
+			<div class="col-md-4 column">
+				<h3>
+					Summer 2014 Lineup
+				</h3>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							PAC Summer
+						</h3>
 					</div>
-					<div class="col-md-4">
-						<div class="thumbnail">
-							<img alt="600x200" src="assets/img/27-thumbnail.jpg" />
-							<div class="caption">
-								<h3>
-									PAC 27+
-								</h3>
-								<p>
-									PAC 27+ is our sub-group that shows anime that otherwise would not fit in the regular lineup due to the length of the programs. It usually features shows which are greater than 27 episodes in length.
-								</p>
-								<p>
-									<a class="btn btn-primary" href="#27">Details</a>
-								</p>
-							</div>
-						</div>
+					<div class="panel-body">
+						<table class="table table-hover table-condensed">
+							<tr>	<th></th>	<th>Show</th>	<th>Episode</th>	</tr>
+							<tr>	<td>1</td>	<td>Koi Choco</td>	<td>-</td>	</tr>
+							<tr>	<td>2</td>	<td>White Album 2</td>	<td>-</td>	</tr>
+							<tr>	<td>3</td>	<td>Kyousougiga</td>	<td>-</td>	</tr>
+							<tr>	<td>4</td>	<td>Samurai Flamenco</td>	<td>-</td>	</tr>
+							<tr>	<td>5</td>	<td>Golden Time</td>	<td>-</td>	</tr>
+							<tr>	<td>6</td>	<td>Outbreak Company</td>	<td>-</td>	</tr>
+						</table>
+					</div>
+					<div class="panel-footer">
+						Thursdays at 7PM
 					</div>
 				</div>
-			</div>
-			<div class="well">
-				<h2>
-					PAC Theater
-				</h2>
-				<div class="responsive-video">
-					<iframe src="http://www.youtube.com/embed/kUDs5pOyWkk" frameborder="0" allowfullscreen=""></iframe>
+				<hr />
+				<h3>
+					Fall 2014 Lineup
+				</h3>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							PAC
+						</h3>
+					</div>
+					<div class="panel-body">
+						<p>To be announced at the callout.</p>
+					</div>
+					<div class="panel-footer">
+						Thursdays at 7PM
+					</div>
 				</div>
-			</div>
-		</div>
-		<div class="col-md-4 column">
-			<h3>
-				Summer 2014 Lineup
-			</h3>
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title">
-						PAC Summer
-					</h3>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							PAC Classic
+						</h3>
+					</div>
+					<div class="panel-body">
+						<p>To be announced at the callout.</p>
+					</div>
+					<div class="panel-footer">
+						Mondays at 7PM
+					</div>
 				</div>
-				<div class="panel-body">
-					<table class="table table-hover table-condensed">
-						<tr>	<th></th>	<th>Show</th>	<th>Episode</th>	</tr>
-						<tr>	<td>1</td>	<td>Koi Choco</td>	<td>-</td>	</tr>
-						<tr>	<td>2</td>	<td>White Album 2</td>	<td>-</td>	</tr>
-						<tr>	<td>3</td>	<td>Kyousougiga</td>	<td>-</td>	</tr>
-						<tr>	<td>4</td>	<td>Samurai Flamenco</td>	<td>-</td>	</tr>
-						<tr>	<td>5</td>	<td>Golden Time</td>	<td>-</td>	</tr>
-						<tr>	<td>6</td>	<td>Outbreak Company</td>	<td>-</td>	</tr>
-					</table>
-				</div>
-				<div class="panel-footer">
-					Thursdays at 7PM
-				</div>
-			</div>
-			<hr />
-			<h3>
-				Fall 2014 Lineup
-			</h3>
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title">
-						PAC
-					</h3>
-				</div>
-				<div class="panel-body">
-				</div>
-				<div class="panel-footer">
-					Thursdays at 7PM
-				</div>
-			</div>
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title">
-						PAC Classic
-					</h3>
-				</div>
-				<div class="panel-body">
-				</div>
-				<div class="panel-footer">
-					Mondays at 7PM
-				</div>
-			</div>
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title">
-						PAC 27+
-					</h3>
-				</div>
-				<div class="panel-body">
-				</div>
-				<div class="panel-footer">
-				   Wednesdays at 7PM
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							PAC 27+
+						</h3>
+					</div>
+					<div class="panel-body">
+						<p>To be announced at the callout.</p>
+					</div>
+					<div class="panel-footer">
+					   Wednesdays at 7PM
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-	<!-- Bootstrap core JavaScript
-	================================================== -->
-	<!-- Placed at the end of the document so the pages load faster -->
+	<!-- Bootstrap core JavaScript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	
   </body>
 </html>
 
