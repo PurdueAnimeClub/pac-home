a8700ac335f2389647e350ef2aecc55ec0f25d95
Sargun Vohra <sargunster@gmail.com>
2014-07-18 15:29:04 -0400

    Loading dialog when ANN data is slow to load

--- sidebar.php
+++ sidebar.php
@@ -71,9 +71,19 @@
 											$episode = $lineup[$x]['episode'];
 											echo '<tr data-target="#infoModal'.$id.'" data-toggle="modal" href="./modal.php?id='.$id.'&name='.str_replace(' ', '+', $name).'" class="anime"><td>'.$slot.'</td><td>'.$name.'</td><td>'.$episode.'</td></tr>';
 											?>
-											<div class="modal fade" id="infoModal<?php echo $id ?>" tabindex="-1" role="dialog" aria-labelledby="infoModalLabel<?php echo $id ?>" aria-hidden="true">
+											<div class="modal fade" id="infoModal<?php echo $id; ?>" tabindex="-1" role="dialog" aria-labelledby="infoModalLabel<?php echo $id; ?>" aria-hidden="true">
 												<div class="modal-dialog">
 													<div class="modal-content">
+														<div class="modal-header">
+															<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+															<h4 class="modal-title" id="infoModalLabel<?php echo $id; ?>">Loading...</h4>
+														</div>
+														<div class="modal-body">
+															Loading...
+														</div>
+														<div class="modal-footer">
+															<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+														</div>
 													</div>
 												</div>
 											</div>	
