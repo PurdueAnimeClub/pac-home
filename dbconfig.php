cd8f3e8984d7475cc22d1ada17c02ad4a5b0fc2e
Sargun Vohra <sargunster@gmail.com>
2014-07-18 22:56:48 -0400

    Remove table stuff from dbconfig.

--- dbconfig.php
+++ dbconfig.php
@@ -3,6 +3,4 @@ $db_address = "localhost";
 $db_user = "testuser";
 $db_password = "testpass";
 $db_name = "testdatabase";
-$table_announcements = "home_announcements";
-$table_config = "home_config";
 ?>
