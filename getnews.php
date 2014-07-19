69d79ed6f83b1d672033b5fe0a0c4392a81a80ab
Sargun Vohra <sargunster@gmail.com>
2014-07-18 21:28:55 -0400

    Announcement titles

--- getnews.php
+++ getnews.php
@@ -5,10 +5,10 @@ function get_news($con, $index) {
 	$row = mysqli_fetch_array($result);
 	if(isset($row)) {
 		$date = date("F d, Y", strtotime($row['Date']));
+		$title = htmlspecialchars($row['Title']);
 		$author = htmlspecialchars($row['Author']);
 		$content = nl2br(htmlspecialchars($row['Content']));
-		
-		$return = '<p><small>'.$date.' by '.$author.'</small></p>'.'<p>'.$content.'</p>';
+		$return = '<h4>'.$title.'</h4><p><small>'.$date.' by '.$author.'</small></p>'.'<p>'.$content.'</p>';
 		return $return;
 	} else {
 		return '';
