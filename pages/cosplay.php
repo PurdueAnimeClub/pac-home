8b37f36754857a5ec23d001f6b4d828d22126191
Sargun Vohra <sargunster@gmail.com>
2014-07-20 18:31:19 -0400

    Load cosplay pics from database

--- pages/cosplay.php
+++ pages/cosplay.php
@@ -6,9 +6,15 @@
 	</p>
 	
 	<div id="links">
-		<a href="http://placehold.it/1600x900/&text=Banana" title="Banana"></a>
-		<a href="http://placehold.it/900x1600/&text=Apple" title="Apple"></a>
-		<a href="http://placehold.it/1000/&text=Orange" title="Orange"></a>
+		<?php
+			$query = "SELECT * FROM home_cosplaypics ORDER BY Position ASC";
+			$result = mysqli_query($con, $query);
+			while ($row = mysqli_fetch_array($result)) {
+				$href = $row['File'];
+				$title = $row['Title'];
+				echo '<a href="'.$href.'" title="'.$title.'"></a>';
+			}
+		?>
 	</div>
 	
 	<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-carousel blueimp-gallery-controls">
